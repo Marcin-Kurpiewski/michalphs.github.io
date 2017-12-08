b1a780cd13e065b96697f1e588594df6bec95653
Michał <michalphs@vp.pl>
2017-12-08 20:18:49 +0100

    update

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,17 @@
-!function(){var e=new Siema({selector:".portfolio__slider",duration:500,perPage:1,startIndex:0,draggable:!0,loop:!0});setInterval(function(){return e.next()},2e4)}();
+(function () {
+
+    const slider = new Siema({
+        selector: '.portfolio__slider',
+        duration: 500,
+        perPage: 1,
+        startIndex: 0,
+        draggable: true,
+        loop: true,
+    });
+    const leftBtn = document.querySelector('.arrow__btn-left').addEventListener('click', function () {
+        return slider.prev();
+    });
+    const rightBtn = document.querySelector('.arrow__btn-right').addEventListener('click', function(){
+        return slider.next();
+    });
+})();
