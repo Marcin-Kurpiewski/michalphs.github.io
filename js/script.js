7b14d7b8f4e793ed0bcd319f02c053906d2127b6
Michał <michalphs@vp.pl>
2017-12-08 20:15:49 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,1 @@
-!function(){var e=document.querySelector(".nav__toggle");e.addEventListener("click",function(){document.querySelector(".nav").classList.toggle("nav--active"),e.classList.toggle("nav__toggle--active")},!1)}();
+!function(){var e=document.querySelector(".nav__toggle");e.addEventListener("click",function(){document.querySelector(".nav").classList.toggle("nav--active"),e.classList.toggle("nav__toggle--active"),e.setAttribute("aria-expanded",!0)},!1)}();
